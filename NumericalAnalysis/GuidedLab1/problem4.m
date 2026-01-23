210374c3ab68da09a818b0a45ea8fa9d8ab9ce10
Rob Tompkins <chtompki@apache.org>
2026-01-23 07:02:48 -0500

    update(NumericalAnalysis): guided lab 1

--- NumericalAnalysis/GuidedLab1/problem4.m
+++ NumericalAnalysis/GuidedLab1/problem4.m
@@ -7,7 +7,7 @@ function [p,absError,relError] = fixpt(a,y,n)
     relError = zeros(1,n);
     for k = 1:n
         p_new = 0.5 * (p + a / p); % Update p using the fixed-point iteration formula
-        absError(k) = abs(p_new - sqrt(a)); % Calculate absolute error
+        absError(k) = abs(p_new - p); % Calculate absolute error
         relError(k) = absError(k) / abs(p_new); % Calculate relative error
         if absError < 1e-10 % Check for convergence
             break;
@@ -18,13 +18,6 @@ end
 
 format long
 [p,absError,relError] = fixpt(2,30,6)
-loglog(1:6,absError, '-k')
-hold on
-loglog(1:6,relError,'--k')
-hold off
-legend("Absolute Error", "Relative Error")
-
-[p,absError,relError] = fixpt(2,1.5,6)
 loglog(1:6,absError, '-k')
 hold on
 loglog(1:6,relError,'--k')
