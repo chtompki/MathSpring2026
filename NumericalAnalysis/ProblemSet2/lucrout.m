60aaab1ca3fb4ea95fc9fae1d5c1ca4fda1a9dee
Rob Tompkins <chtompki@apache.org>
2026-02-22 22:45:32 -0500

    update(NumericalAnalysis): ProblemSet2 problem 4

--- NumericalAnalysis/ProblemSet2/lucrout.m
+++ NumericalAnalysis/ProblemSet2/lucrout.m
@@ -1,16 +1,19 @@
 function [L,U] = lucrout(A)
-    n = length(A);
-    L = zeros(size(A));
-    U = zeros(size(A));
-    L(:,1) = A(:,1);
-    U(:,1) = A(:,1)/L(1,1);
-    U(1,1) = 1;
-    for k = 2:n
-        for j = 2:n
-            for i = j:n
-                L(i,j) = A(i,j) - dot(L(i,1:j-1),U(1:j-1,j));
-            end
-            U(k,j) = (A(k,i) - dot(L(k,1:k-1),U(1:k-1,j))) / L(k,k);
+    n = size(A,1);
+    if size(A,2) ~= n
+        error('A must be square');
+    end
+    L = zeros(n,n);
+    U = eye(n);
+    for j = 1:n
+        for i = j:n
+            L(i,j) = A(i,j) - L(i,1:j-1)*U(1:j-1,j);
+        end
+        if L(j,j) == 0
+            error('Zero pivot at j=%d (need pivoting).', j);
+        end
+        for k = j+1:n
+            U(j,k) = (A(j,k) - L(j,1:j-1)*U(1:j-1,k)) / L(j,j);
         end
     end
 end
